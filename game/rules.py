872420de236fc5d935a91a7a958834015dac48ec
Wesam-91 <wesam.ayman@gmail.com>
2026-05-14 17:07:47 +0300

    Added wall movement constrains

--- game/rules.py
+++ game/rules.py
@@ -1,15 +1,36 @@
 from game.constants import BOARD_SIZE
 
-
 def is_inside_board(row, col):
     return 0 <= row < BOARD_SIZE and 0 <= col < BOARD_SIZE
 
+
+def is_wall_blocking(board, r, c, dr, dc):
+    """
+    check if a wall prevents movement from cell (r, c) in direction (dr, dc).
+    """
+    if dr == -1:  # Move UP
+        if (r - 1, c - 1) in board.horizontal_walls or (r - 1, c) in board.horizontal_walls:
+            return True
+    elif dr == 1:  # Move DOWN
+        if (r, c - 1) in board.horizontal_walls or (r, c) in board.horizontal_walls:
+            return True
+    elif dc == -1:  # Move LEFT
+        if (r - 1, c - 1) in board.vertical_walls or (r, c - 1) in board.vertical_walls:
+            return True
+    elif dc == 1:  # Move RIGHT
+        if (r - 1, c) in board.vertical_walls or (r, c) in board.vertical_walls:
+            return True
+
+    return False
 
 def resolve_move(board, player_id, dr, dc):
     row, col = board.get_player_position(player_id)
 
     opponent_id = 2 if player_id == 1 else 1
     opp_row, opp_col = board.get_player_position(opponent_id)
+
+    if is_wall_blocking(board, row, col, dr, dc):
+        return None
 
     next_pos = (row + dr, col + dc)
 
@@ -25,9 +46,9 @@ def resolve_move(board, player_id, dr, dc):
     # Jump Over Opponent
     # =========================
     jump_pos = (opp_row + dr, opp_col + dc)
-
+    jump_blocked = is_wall_blocking(board, opp_row, opp_col, dr, dc)
     # لو القفزة متاحة
-    if is_inside_board(*jump_pos):
+    if is_inside_board(*jump_pos) and not jump_blocked:
         return jump_pos
 
     # =========================
@@ -40,10 +61,10 @@ def resolve_move(board, player_id, dr, dc):
         diagonal_left = (opp_row, opp_col - 1)
         diagonal_right = (opp_row, opp_col + 1)
 
-        if is_inside_board(*diagonal_left):
+        if is_inside_board(*diagonal_left) and not is_wall_blocking(board, opp_row, opp_col, 0, -1):
             return diagonal_left
 
-        if is_inside_board(*diagonal_right):
+        if is_inside_board(*diagonal_right) and not is_wall_blocking(board, opp_row, opp_col, 0, 1):
             return diagonal_right
 
     # حركة أفقية
@@ -52,10 +73,10 @@ def resolve_move(board, player_id, dr, dc):
         diagonal_up = (opp_row - 1, opp_col)
         diagonal_down = (opp_row + 1, opp_col)
 
-        if is_inside_board(*diagonal_up):
+        if is_inside_board(*diagonal_up) and not is_wall_blocking(board, opp_row, opp_col, -1, 0):
             return diagonal_up
 
-        if is_inside_board(*diagonal_down):
+        if is_inside_board(*diagonal_down) and not is_wall_blocking(board, opp_row, opp_col, 1, 0):
             return diagonal_down
 
     return None
